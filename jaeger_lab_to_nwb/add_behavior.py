620cffb0e67ccb16fa4bdc9cefb0cb0e47351e66
luiztauffer <luiz_tauffer@hotmail.com>
2020-01-30 17:48:11 +0700

    reads trials summary in a pandas df

--- jaeger_lab_to_nwb/add_behavior.py
+++ jaeger_lab_to_nwb/add_behavior.py
@@ -2,12 +2,17 @@ from pynwb import NWBFile, NWBHDF5IO
 from pynwb.device import Device
 
 import numpy as np
+import pandas as pd
 
 
 def add_behavior_labview(nwbfile, source_dir, metadata):
     """
-    Reads behavioral data from CSV files and adds it to nwbfile.
+    Reads behavioral data from txt files and adds it to nwbfile.
     """
+
+    fname = 'GPi4_020619_AP3_4_OPTO_tr.txt'
+    fpath = os.path.join(source_dir, fname)
+    df_trials_summary = pd.read_csv(fpath, sep='\t', index_col=False)
 
     # Adds Device
     device = nwbfile.create_device(name=metadata['Behavior']['Device'][0]['name'])
