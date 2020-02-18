0457f53acafd4e328178fc833f0e78bed7293475
luiztauffer <luiz_tauffer@hotmail.com>
2020-02-18 15:39:36 +0700

    tests if subject metadata exists

--- jaeger_lab_to_nwb/resources/create_nwbfile.py
+++ jaeger_lab_to_nwb/resources/create_nwbfile.py
@@ -4,20 +4,22 @@ from pynwb.file import Subject
 
 def create_nwbfile(metadata):
     """Creates a new NWBFile object with specific metadata information."""
+
     # Initialize a NWB object
     nwbfile = NWBFile(**metadata['NWBFile'])
 
     # Add subject metadata
-    experiment_subject = Subject(
-        age=metadata['Subject']['age'],
-        subject_id=metadata['Subject']['subject_id'],
-        species=metadata['Subject']['species'],
-        description=metadata['Subject']['description'],
-        genotype=metadata['Subject']['genotype'],
-        date_of_birth=metadata['Subject']['date_of_birth'],
-        weight=metadata['Subject']['weight'],
-        sex=metadata['Subject']['sex']
-    )
-    nwbfile.subject = experiment_subject
+    if 'Subject' in metadata:
+        experiment_subject = Subject(
+            age=metadata['Subject']['age'],
+            subject_id=metadata['Subject']['subject_id'],
+            species=metadata['Subject']['species'],
+            description=metadata['Subject']['description'],
+            genotype=metadata['Subject']['genotype'],
+            date_of_birth=metadata['Subject']['date_of_birth'],
+            weight=metadata['Subject']['weight'],
+            sex=metadata['Subject']['sex']
+        )
+        nwbfile.subject = experiment_subject
 
     return nwbfile
