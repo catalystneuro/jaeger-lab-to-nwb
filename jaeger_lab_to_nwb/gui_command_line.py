0e58f9b2ae4fa2aeae74780dbccf1a711614cf81
luiztauffer <luiz_tauffer@hotmail.com>
2020-03-02 15:47:44 +0700

    - experiments as packages stored in same place - fix make_env.yml - install yml files with pip

--- jaeger_lab_to_nwb/gui_command_line.py
+++ jaeger_lab_to_nwb/gui_command_line.py
@@ -20,11 +20,11 @@ def main():
         experiment = sys.argv[1]
 
         # Tests if module exists
-        module_spec = importlib.util.find_spec("jaeger_lab_to_nwb." + experiment)
+        module_spec = importlib.util.find_spec("jaeger_lab_to_nwb.experiments." + experiment)
         if module_spec is not None:
             print("Importing experiment configuration from " + experiment)
             # Generating the submodule name from string
-            full_module_name = "jaeger_lab_to_nwb." + experiment + ".nwbn_gui"
+            full_module_name = "jaeger_lab_to_nwb.experiments." + experiment + ".nwbn_gui"
 
             # Import gui submodule
             gui = importlib.import_module(full_module_name)
@@ -33,7 +33,7 @@ def main():
             print("Running nwbn-gui for experiment: ", experiment)
             gui.main()
         else:
-            print("Module" + experiment + " does not exist. Running nwbn-gui without specific metadata.")
+            print("Module " + experiment + " does not exist. Running nwbn-gui without specific metadata.")
             from nwbn_conversion_tools.gui import command_line as gui
             gui.main()
     else:
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='jaeger_lab_to_nwb',
-    version='0.0.1',
+    version='0.1.0',
     description='NWB conversion scripts and tutorials.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -15,9 +15,10 @@ setup(
     email='ben.dichter@gmail.com',
     packages=find_packages(),
     include_package_data=True,
+    package_data={'': ['*.yml']},
     install_requires=[
         'matplotlib', 'cycler', 'scipy', 'numpy', 'jupyter', 'h5py', 'pynwb',
-        'pyintan'],
+        'pyintan', 'nwbn-conversion-tools', 'ndx-fret'],
     entry_points={
         'console_scripts': ['nwbn-gui-jaeger=jaeger_lab_to_nwb.gui_command_line:main'],
     }
