9d849cc9dcfea109032d7a55c47adea040993ec1
luiz <luiz_tauffer@otmail.com>
2020-12-03 16:21:44 +0100

    intan datainterface

--- jaeger_lab_to_nwb/treadmillconverter/treadmillconverter.py
+++ jaeger_lab_to_nwb/treadmillconverter/treadmillconverter.py
@@ -1,13 +1,15 @@
 from nwb_conversion_tools import NWBConverter
 from nwb_conversion_tools.json_schema_utils import dict_deep_update
 from .treadmilldatainterface import TreadmillDataInterface
+from .intandatainterface import IntanDataInterface
 from pathlib import Path
 import yaml
 
 
 class JaegerTreadmillConverter(NWBConverter):
     data_interface_classes = dict(
-        TreadmillDataInterface=TreadmillDataInterface
+        TreadmillDataInterface=TreadmillDataInterface,
+        IntanDataInterface=IntanDataInterface
     )
 
     def get_metadata(self):
@@ -18,5 +20,6 @@ class JaegerTreadmillConverter(NWBConverter):
             metadata = yaml.safe_load(f)
 
         metadata = dict_deep_update(metadata, self.data_interface_objects['TreadmillDataInterface'].get_metadata())
+        metadata = dict_deep_update(metadata, self.data_interface_objects['IntanDataInterface'].get_metadata())
 
         return metadata
